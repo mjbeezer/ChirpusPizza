565afc14360637825cf05ddd9251960047e065c2
Matt B <mjbeezer11621@gmail.com>
2021-11-11 15:53:47 -0500

    pizza pizza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'menu';
+
+  items: MenuItem[] = 
+    [
+      {name: "Chicken Fingers", category: "main dish", price: 11.99 },
+      {name: "Pizza", category: "main dish", price: 11.99 },
+      {name: "Wings", category: "side", price: 8.99 },
+      {name: "Breadsticks", category: "side", price: 4.99 },
+      {name: "Caeser Salad", category: "salad", price: 5.99 },
+      {name: "Cinnastix", category: "dessert", price: 8.99 }
+    ]    
 }
